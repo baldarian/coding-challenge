fbad323dd3a2a02cad3208d9b4d937b27f1871f0
Hovhannes <john.baldarian@gmail.com>
2021-02-13 00:16:45 +0400

    Create example1.ts

---
+++ example1.ts
@@ -0,0 +1,24 @@
+interface Props {
+  items: Item[];
+}
+
+interface Item {
+  id: string;
+  title: string;
+  description: string;
+}
+
+const Component: FC<Props> = ({ items }) => {
+  return (
+    <div>
+      {items.map((item, index) => 
+        <Item
+          // It looks fine untill we introduce reording functionality
+          // Either way, passing `id` here is much better
+          key={index}
+          item={item}
+        />
+      )}
+    </div>
+  );
+};
